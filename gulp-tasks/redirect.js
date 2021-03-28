b613d7ec7db3bf3cfddc359cade0c07cf4c1cbcf
Johnny Henshall <6359748+jhenshall@users.noreply.github.com>
2021-03-28 23:16:49 +0100

    Redirects to /en pages

--- gulp-tasks/redirect.js
+++ gulp-tasks/redirect.js
@@ -2,10 +2,18 @@
 
 module.exports = function (gulp) {
     const rename = require('gulp-rename');
+    const replace = require('gulp-replace');
+    var pages = ['about.html','cookies.html','data.html','get-involved.html','index.html','privacy.html','project.html']
 
     return function () {
-        return gulp.src('./redirect.shtml')
-                .pipe(rename("index.html"))
-                .pipe(gulp.dest('./docs/'));
+    	var output = [];
+    	pages.forEach((page)=>{
+
+	        output.push(gulp.src('./redirect.shtml')
+                .pipe(rename(page))
+                .pipe(replace(/PAGENAME/g, page))
+                .pipe(gulp.dest('./docs/'))
+	        );
+        });
     };
 };
